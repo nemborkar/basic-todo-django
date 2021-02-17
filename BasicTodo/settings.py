ebfd8fe1bf9e146ecfdc3536ea55d72abb690957
nemborkar <swarnimborkar@gmail.com>
2021-02-17 18:08:53 +0900

    added mysql database

--- BasicTodo/settings.py
+++ BasicTodo/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'BasicTodo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS':{
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- BasicTodo/urls.py
+++ BasicTodo/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from todo_list.views import todos_view, home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', home, name="home"),
-    path('todos/', todos_view),
+    path('todos/', todos_view, name="todos"),
+    path('admin/', admin.site.urls),
 ]
