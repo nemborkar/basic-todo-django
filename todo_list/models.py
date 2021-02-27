e266ec299333a0d9664ee0cd56b82855abd4e105
nemborkar <swarnimborkar@gmail.com>
2021-02-27 23:07:17 +0900

    finalized models

--- todo_list/models.py
+++ todo_list/models.py
@@ -5,7 +5,9 @@ from datetime import datetime
 
 class Todo(models.Model):
 	title = models.CharField(max_length=200)
-	# details = models.TextField(blank=True)
-	created_at = models.DateTimeField(default=datetime.now, blank=True)
+	completed = models.BooleanField(default=False)
+	# created_at = models.DateTimeField(default=datetime.now, blank=True)
+	
 	def __str__(self):
-		return self.title
+		# return self.title
+		return self.title + ' | ' + str(self.completed)
