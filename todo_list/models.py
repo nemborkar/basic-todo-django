91bb6ad0d3049b59174dd61fcb8117c2b0a10dac
nemborkar <swarnimborkar@gmail.com>
2021-02-27 22:33:03 +0900

    minor change to model

--- todo_list/models.py
+++ todo_list/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 class Todo(models.Model):
 	title = models.CharField(max_length=200)
-	details = models.TextField()
+	# details = models.TextField(blank=True)
 	created_at = models.DateTimeField(default=datetime.now, blank=True)
 	def __str__(self):
 		return self.title
